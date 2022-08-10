54f2a1877dd911a332178f92fac80b07811cb12b
MultiGitTest <multigittest@gmail.com>
2022-08-10 09:52:33 -0400

    Initialising ASP.NET web application MVC named BookRecommender

---
+++ BooksRecommender/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksRecommender.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
